68124341b0336eaf7da3cfb413c2b767b72a5b7b
Vaibhav-Kambli <51274039+Vaibhav-Kambli@users.noreply.github.com>
2020-03-08 23:30:27 -0400

    Add files via upload
    Calendar module in Python

---
+++ Calendar/displayCalendar.py
@@ -0,0 +1,11 @@
+import calendar
+
+year = 2020
+month = 3
+
+# for user input of year and month use the following block of code
+# year = int(input("Enter year : "))
+# month = int(input("Enter month : "))
+
+#To display calendar
+print(calendar.month(year, month))
